e506cfba510394a260ed7742cc7ef29df373ce36
Karyna Rebrishcheva <karyna_rebrishcheva@epam.com>
2021-09-26 16:32:09 +0300

    Configure hilt testing

---
+++ app/src/androidTest/java/com/example/musicapp/ui/main/view/MainFragmentTest.kt
@@ -0,0 +1,38 @@
+package com.example.musicapp.ui.main.view
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.example.musicapp.R
+import com.example.musicapp.util.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class MainFragmentTest{
+    @get:Rule
+    var hiltAndroidRule = HiltAndroidRule(this)
+
+    @Before
+    fun setUp() {
+        hiltAndroidRule.inject()
+    }
+
+    @Test
+    fun test_initFragment() {
+        launchFragmentInHiltContainer<MainFragment>()
+    }
+
+    private fun scrollAtAndCheckTestVisible(position: Int, text: String) {
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerTracks))
+            .perform(
+                RecyclerViewActions
+                    .scrollToPosition<TrackAdapter.TrackViewHolder>(position))
+        Espresso.onView(ViewMatchers.withText(text))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
