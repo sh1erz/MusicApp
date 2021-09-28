401bacce5dd34cd8c643e6d4492d59d36f9c37f2
Karyna Rebrishcheva <karyna_rebrishcheva@epam.com>
2021-09-28 18:47:33 +0300

    Theme SharedPreferences

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.musicapp
 
+import android.content.SharedPreferences
+import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,21 +10,29 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 import com.example.musicapp.databinding.ActivityMainBinding
 import com.example.musicapp.ui.main.model.LOG
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+    private lateinit var binding: ActivityMainBinding
+    private var theme: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        theme =
+            PreferenceManager.getDefaultSharedPreferences(application).getString("theme", "Light")!!
+        when(theme){
+            "Light" -> setTheme(R.style.Theme_MusicApp)
+            "Classic" -> setTheme(R.style.Theme_MusicApp_Classic)
+            else -> setTheme(R.style.Theme_MusicApp)
+        }
         setContentView(binding.root)
         Log.i(LOG, "onCreate: activity")
-
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.navHostFragment).apply {
             addOnDestinationChangedListener { _, _, args ->
@@ -40,6 +50,37 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "theme") {
+            theme =
+                PreferenceManager.getDefaultSharedPreferences(application).getString(key, "Light")!!
+            setTheme(theme)
+
+        }
+    }
+
+    override fun getTheme(): Resources.Theme {
+        val theme = super.getTheme()
+        val themeName = this.theme ?: PreferenceManager.getDefaultSharedPreferences(application)
+            .getString("theme", "Light")!!
+        when (themeName) {
+            "Light" -> theme.applyStyle(R.style.Theme_MusicApp, true)
+            "Classic" -> theme.applyStyle(R.style.Theme_MusicApp_Classic, true)
+            else -> theme.applyStyle(R.style.Theme_MusicApp, true)
+        }
+        return theme
+    }
+
+    private fun setTheme(themeName: String?) {
+        val preferences =
+            PreferenceManager.getDefaultSharedPreferences(application)
+        preferences.edit().apply{
+            putString("theme", themeName)
+            apply()
+        }
+        recreate()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/musicapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/musicapp/ui/settings/SettingsFragment.kt
@@ -2,11 +2,24 @@ package com.example.musicapp.ui.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.example.musicapp.MainActivity
 import com.example.musicapp.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences
+            .registerOnSharedPreferenceChangeListener(activity as MainActivity)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        preferenceScreen.sharedPreferences
+            .unregisterOnSharedPreferenceChangeListener(activity as MainActivity)
     }
 }
